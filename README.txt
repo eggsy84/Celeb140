0d437a10307d1a813aa04183f8a313f64a69cc0f
eggsy84 <eggsy@eggsylife.co.uk>
2011-10-15 12:27:26 +0100

    Initial share of Celeb140 project

---
+++ README.txt
@@ -0,0 +1,5 @@
+Celeb 140 Android App
+---------------------
+
+Android app for browing the tweets from Celebrity Twitter users.
+
---
+++ src/uk/co/celeb140/Celeb140Activity.java
@@ -0,0 +1,13 @@
+package uk.co.celeb140;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Celeb140Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
