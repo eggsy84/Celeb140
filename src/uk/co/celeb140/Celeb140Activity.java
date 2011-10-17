e5c642305f47b00c662020229703c3239aeefa75
eggsy <eggsy@eggsylife.co.uk>
2011-10-17 20:58:59 +0100

    Add some basic click handling and dummy holding icons.

--- src/uk/co/celeb140/Celeb140Activity.java
+++ src/uk/co/celeb140/Celeb140Activity.java
@@ -1,7 +1,10 @@
 package uk.co.celeb140;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Celeb140Activity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,36 @@ public class Celeb140Activity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.celeb140_dashboard);
+    }
+    
+    public void onClickFeature(View view){
+    	int id = view.getId ();
+    	
+    	Context context = getApplicationContext();
+		CharSequence text = null;
+		int duration = Toast.LENGTH_SHORT;
+    	
+    	switch (id) {
+    		case R.id.home_button_fame_hungry: {
+    			text = "Fame hungry pressed";
+    			break;
+    		}
+    		case R.id.home_button_guest_list: {
+    			text = "Guest list pressed";
+    			break;
+    		}
+    		case R.id.home_button_papped: {
+    			text = "Papped pressed";
+    			break;
+    		}
+    		case R.id.home_button_classified: {
+    			text = "Classified pressed";
+    			break;
+    		}
+    	}
+    	
+    	Toast toast = Toast.makeText(context, text, duration);
+		toast.show();
+    	
     }
 }
